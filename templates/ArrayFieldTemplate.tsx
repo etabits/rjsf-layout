eb8d37bf22b8a92c2a02ba3a2ffa7c38903a2050
Hasan Arous <arous.hasan@gmail.com>
2023-06-05 09:47:40 +0300

    ArrayFieldTemplate stub (unused yet!)

---
+++ templates/ArrayFieldTemplate.tsx
@@ -0,0 +1,30 @@
+import type { TemplatesType } from "@rjsf/utils";
+import { useContext } from "react";
+import LayoutContext from "../contexts/Layout";
+import { getDefaultRegistry } from "@rjsf/core";
+import DisplayLayout from "../utils/DisplayLayout";
+
+const DefaultArrayFieldTemplate =
+  getDefaultRegistry().templates["ArrayFieldTemplate"];
+
+const ArrayFieldTemplate: TemplatesType["ArrayFieldTemplate"] = (props) => {
+  const { layout, theme } = useContext(LayoutContext);
+
+  const RegArrayFieldTemplate =
+    theme?.templates?.["ArrayFieldTemplate"] || DefaultArrayFieldTemplate;
+
+  const expandedLayout =
+    typeof layout === "function"
+      ? layout({
+          Field,
+        })
+      : layout;
+
+  return expandedLayout ? (
+    DisplayLayout({ layout: expandedLayout })
+  ) : (
+    <RegArrayFieldTemplate {...props} />
+  );
+};
+
+export default ArrayFieldTemplate;
