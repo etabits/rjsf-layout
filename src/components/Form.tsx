32fc67e7a82f4d95b4a5c8596f8300fd2cbfdccf
Hasan Arous <arous.hasan@gmail.com>
2023-09-26 12:45:43 +0300

    memoize theme-ful <Form to avoid re-renders
    and regain lost performance, of course!

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,6 +4,7 @@ import type { JSONSchemaObject, LayoutFormProps } from "../types";
 import LayoutContext from "../contexts/Layout";
 import FieldTemplate from "./templates/Field";
 import ObjectFieldTemplate from "./templates/ObjectField";
+import { useMemo } from "react";
 
 const Form = <S extends JSONSchemaObject>({
   children,
@@ -12,7 +13,10 @@ const Form = <S extends JSONSchemaObject>({
   ...props
 }: LayoutFormProps<S>) => {
   const rjsfProps = props as FormProps;
-  const RJSFForm = theme ? withTheme(theme) : DefaultForm;
+  const RJSFForm = useMemo(
+    () => (theme ? withTheme(theme) : DefaultForm),
+    [theme]
+  );
 
   if (!children) {
     // FIXME need to apply theme stuff here!
